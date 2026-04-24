6377c785b7eb53607ec14107f54cff5c32d348d1
alcantar87 <alcantar87@gmail.com>
2026-04-24 11:35:18 -0600

    Preparar Django para deploy en Render

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w@9iq#(+gfv+h1$sm8f(3o@uxiy$#xp3(u72&!h4u%t$$-guy(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
